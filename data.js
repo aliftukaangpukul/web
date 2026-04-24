74bc0dd0304a754f5edc548cd6d7b8b4f2043f63
alif <rafifialif77@gmail.com>
2026-04-24 09:22:33 +0700

    upload github pertamaku

---
+++ data.js
@@ -0,0 +1,11 @@
+const databaseProyek = {
+    "sistem_pos": {
+        judul: "SISTEM POS UNTUK PT PERAIRAN",
+        keahlian: ["Javascript"],
+        gambar: "https://preview.redd.it/ai-or-real-v0-haa399u8zs5d1.jpeg?auto=webp&s=0a5ae54c4a3ada7f83f7e1c98ce44e129e2990df",
+        latar: "ini adalah proyek magang dimana saya membuat sistem pos untuk PT.PERAIRAN medan",
+        tantangan: " bisa masuk angin di pt ini ",
+        pelajaran: "saya mendapatkan pelajaran",
+        link: ""
+    },
+};
